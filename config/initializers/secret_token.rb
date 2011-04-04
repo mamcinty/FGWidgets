f0b64970ff342f09e40ef4440d2de8d9bbfaa0be
Matt McIntyre <matt@stillcompiling.com>
2011-04-04 19:20:19 -0400

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FGWidgets::Application.config.secret_token = 'cf97b1c076e6e708bc12cdcb89edfce955411d70d899767e6903f6d6aa25df7b7e76e8ea43ebb37768b020962470c0c3fb0f5193319012279398e5eb1b920075'
