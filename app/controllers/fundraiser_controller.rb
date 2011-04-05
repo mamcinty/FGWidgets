67e3ea545d69a7a2233d663ce3498b5bd310a3db
Matt McIntyre <matt@stillcompiling.com>
2011-04-04 23:27:31 -0400

    Controller now responds to XML

--- app/controllers/fundraiser_controller.rb
+++ app/controllers/fundraiser_controller.rb
@@ -1,6 +1,11 @@
 class FundraiserController < ApplicationController
   def show
     @fundraiser = Fundraiser.find(params[:profile_name], params[:fundraiser_name])
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @fundraiser }
+    end
   end
 
 end
