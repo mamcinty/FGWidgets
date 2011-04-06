dbe0eea48bb3e4388f5d2a264c463678add74015
Matt McIntyre <matt@stillcompiling.com>
2011-04-06 16:28:30 -0400

    Renamed trackers.html.erb to tracker.html.erb and specified to render tracker.html.erb on show action of TrackersController

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -15,7 +15,7 @@ class TrackersController < ApplicationController
   def show
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html { render :layout => 'tracker' } # show.html.erb
       format.xml  { render :xml => @tracker }
     end
   end
